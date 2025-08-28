4aecffbe005b5aa8185a58a86547e1b19e51390d
Jack Doyle <jdoyle10@huskers.unl.edu>
2025-08-28 09:49:18 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Jack Doyle
+ *         jdoyle10@unl.edu
+ * Date: 2025/08/26
+ *
+ * A Hack in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jack Doyle\n");
+
+  return 0;
+}
